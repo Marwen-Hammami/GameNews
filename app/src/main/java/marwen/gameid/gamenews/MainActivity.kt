04f713ac9032d02dff0a9e0702749c9cee8924e4
Marwen-Hammami <hammamimarwen.d@gmail.com>
2024-10-06 20:50:12 +0100

    Main UI List

--- app/src/main/java/marwen/gameid/gamenews/MainActivity.kt
+++ app/src/main/java/marwen/gameid/gamenews/MainActivity.kt
@@ -1,47 +1,150 @@
 package marwen.gameid.gamenews
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import marwen.gameid.gamenews.ui.theme.GameNewsTheme
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.size.Size
+import kotlinx.coroutines.flow.collectLatest
+import marwen.gameid.gamenews.data.GameNewsRepositoryImpl
+import marwen.gameid.gamenews.data.model.Newsitem
+import marwen.gameid.gamenews.presentation.GameNewsViewModel
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<GameNewsViewModel>(factoryProducer = {
+        object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return GameNewsViewModel(GameNewsRepositoryImpl(RetrofitInstance.api)) as T
+            }
+        }
+    })
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            GameNewsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                val newsList = viewModel.gameNews.collectAsState().value
+                val context = LocalContext.current
+
+                LaunchedEffect(key1 = viewModel.showErrorToastChannel) {
+                    viewModel.showErrorToastChannel.collectLatest { show ->
+                        if (show){
+                            Toast.makeText(
+                                context, "Error with data in Main", Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
                 }
+
+                if (newsList.isEmpty()){
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        CircularProgressIndicator()
+                    }
+                } else {
+                    LazyColumn(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        contentPadding = PaddingValues(16.dp)
+                    ) {
+                        items(newsList.size){ index ->
+                            Newsitem(newsList[index])
+                            Spacer(modifier = Modifier.height(16.dp))
+                        }
+                    }
+                }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Newsitem(newsitem: Newsitem){
+    val defaultImage = "https://steamcdn-a.akamaihd.net/steam/apps/" + newsitem.appid + "/header.jpg"
+    val imageState = rememberAsyncImagePainter(
+        model = ImageRequest.Builder(LocalContext.current).data(defaultImage)
+            .size(Size.ORIGINAL).build()
+    ).state
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GameNewsTheme {
-        Greeting("Android")
+    Column(
+        modifier = Modifier.clip(RoundedCornerShape(20.dp))
+            .height(300.dp)
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.primaryContainer)
+    ) {
+        if (imageState is AsyncImagePainter.State.Error){
+            Box(modifier = Modifier.fillMaxWidth().height(200.dp), contentAlignment = Alignment.Center) {
+                CircularProgressIndicator()
+            }
+        }
+
+        if (imageState is AsyncImagePainter.State.Success){
+            Image(
+                modifier = Modifier.fillMaxWidth().height(200.dp),
+                painter = imageState.painter,
+                contentDescription = newsitem.title,
+                contentScale = ContentScale.Crop
+                )
+        }
+
+        Spacer(modifier = Modifier.height(6.dp))
+
+        // News title
+        Text(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            text = newsitem.title,
+            fontSize = 17.sp,
+            fontWeight = FontWeight.SemiBold
+        )
+
+        Spacer(modifier = Modifier.height(6.dp))
+
+        // News description
+        Text(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            text = newsitem.contents,
+            fontSize = 13.sp
+        )
+
+
     }
 }
